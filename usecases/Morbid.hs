78f996a128f7e8ccc25c94b13c9236b0d68fb95d
Kiki Ariady Putra <aurodeus@gmail.com>
2022-08-24 14:31:56 +0700

    Submit deadline into datum

--- usecases/Morbid.hs
+++ usecases/Morbid.hs
@@ -24,7 +24,7 @@ A dead-man's switch contract where you can Create Chest and
 the chest can only be unlocked after 30 days of not being
 postponed by the creator. You can also Add Treasure to the
 chest, and of course Delay Unlock by 30 days. Anyone can
-redeem the ADA as long as the chest is unlockedable.
+redeem the treasure when the chest has passed the deadline.
 -}
 import           Control.Lens            (view)
 import           Control.Monad           (void, when)
@@ -54,8 +54,8 @@ import           Plutus.Contract
 import           Plutus.Contract.Test
 import qualified Plutus.Contract.Typed.Tx as Typed
 import qualified PlutusTx
-import           PlutusTx.Prelude         hiding (Semigroup (..), fold)
-import qualified Prelude                  as Haskell (Semigroup (..), show)
+import           PlutusTx.Prelude
+import qualified Prelude                  as Haskell
 
 ------------------------------------------------------------
 
@@ -65,19 +65,49 @@ type AddTreasure = Value
 type DelayUnlock = ()
 type UnlockChest = ()
 
+-- | Datum parameters
+newtype ChestDatum
+    = ChestDatum
+    { _dDeadline :: POSIXTime
+    }
+    deriving (Show)
+    deriving newtype
+    ( PlutusTx.FromData
+    , PlutusTx.ToData
+    , PlutusTx.UnsafeFromData
+    )
+PlutusTx.makeLift ''ChestDatum
+
+-- | Redeemer parameters
+newtype ChestRedeemer
+    = ChestRedeemer ()
+    deriving (Show)
+    deriving newtype
+    ( PlutusTx.FromData
+    , PlutusTx.ToData
+    , PlutusTx.UnsafeFromData
+    )
+PlutusTx.makeLift ''ChestRedeemer
+
 data Morbid
 instance Scripts.ValidatorTypes Morbid where
-    type instance DatumType     Morbid = ()
-    type instance RedeemerType  Morbid = ()
+    type instance DatumType     Morbid = ChestDatum
+    type instance RedeemerType  Morbid = ChestRedeemer
 
 {-# INLINABLE validate #-}
-validate :: () -> () -> ScriptContext -> Bool
-validate _ _ sc = True
+validate :: ChestDatum -> ChestRedeemer -> ScriptContext -> Bool
+validate datum redeemer context = True
 
 typedValidator :: Scripts.TypedValidator Morbid
 typedValidator = Scripts.mkTypedValidator @Morbid
-    $$(PlutusTx.compile [||         validate      ||])
-    $$(PlutusTx.compile [|| Scripts.wrapValidator ||])
+    $$(PlutusTx.compile [|| validate ||])
+    $$(PlutusTx.compile [||   wrap   ||])
+    where wrap = Scripts.wrapValidator
+                @ChestDatum
+                @ChestRedeemer
+            
+        
+    
 
 contractAddress :: Ledger.Address
 contractAddress = Scripts.validatorAddress typedValidator
@@ -93,39 +123,29 @@ morbidContract = selectList
 -- | The "Create Chest" contract endpoint
 createChest :: AsContractError x => Promise () MorbidSchema x ()
 createChest = endpoint @"1. Create Chest" $ \ initialDeposit -> do
-    let tvl = Constraints.typedValidatorLookups typedValidator
-        txn = Constraints.mustPayToTheScript () initialDeposit
-    constraints <- mkTxConstraints tvl txn
-    
-    let aut = Constraints.adjustUnbalancedTx constraints
-    void $ submitUnbalancedTx aut
+    let you = ChestDatum $ TimeSlot.scSlotZeroTime def + 2_592_000_000 -- 30 x 24 x 3600 x 1000ms
+        txn = you `Constraints.mustPayToTheScript` initialDeposit
+    logInfo @Haskell.String $ "Creating chest for " ++ Haskell.show you
+    void $ submitTxConstraints typedValidator txn
 
--- | The "Add Treasure" contract endpoint.
+-- | The "Add Treasure" contract endpoint
 addTreasure :: AsContractError x => Promise () MorbidSchema x ()
 addTreasure = endpoint @"2. Add Treasure" $ \ deposit -> do
-    -- let tvl = Constraints.typedValidatorLookups typedValidator
-    --     txn = Constraints.mustPayToTheScript () deposit
-    -- constraints <- mkTxConstraints tvl txn
-    
-    -- let aut = Constraints.adjustUnbalancedTx constraints
-    -- void $ submitUnbalancedTx aut
     error ()
 
--- | The "Delay Unlock" contract endpoint.
+-- | The "Delay Unlock" contract endpoint
 delayUnlock :: AsContractError x => Promise () MorbidSchema x ()
 delayUnlock = endpoint @"3. Delay Unlock" $ \ _ -> do
-    -- utxoS  <- utxosAt contractAddress
-    
-    -- let txn = collectFromScript utxoS ()
-    -- void $ submitTxConstraintsSpending typedValidator utxoS txn
     error ()
 
--- | The "Unlock Chest" contract endpoint.
+-- | The "Unlock Chest" contract endpoint
 unlockChest :: AsContractError x => Promise () MorbidSchema x ()
 unlockChest = endpoint @"4. Unlock Chest" $ \ _ -> do
     utxoS  <- utxosAt contractAddress
     
-    let txn = collectFromScript utxoS ()
+    let you = ChestRedeemer ()
+        txn = collectFromScript utxoS you
+    logInfo @Haskell.String $ "Unlocking chest"
     void $ submitTxConstraintsSpending typedValidator utxoS txn
 
 endpoints :: AsContractError x => Contract () MorbidSchema x ()
