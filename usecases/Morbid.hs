d12465a8c523b5c7eb1fd2f9e32b2d534b2bbdf1
Kiki Ariady Putra <aurodeus@gmail.com>
2022-08-24 12:46:11 +0700

    Change base scaffolding again to Starter contract

--- usecases/Morbid.hs
+++ usecases/Morbid.hs
@@ -12,6 +12,11 @@
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeOperators         #-}
 
+{-# OPTIONS_GHC -fno-ignore-interface-pragmas   #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports        #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches        #-}
+{-# OPTIONS_GHC -fno-warn-redundant-constraints #-}
+
 module Morbid where
 
 {- DAVY JONES' LOCKER
@@ -50,181 +55,86 @@ import           Plutus.Contract.Test
 import qualified Plutus.Contract.Typed.Tx as Typed
 import qualified PlutusTx
 import           PlutusTx.Prelude         hiding (Semigroup (..), fold)
-import           Prelude                  as Haskell (Semigroup (..), show)
+import qualified Prelude                  as Haskell (Semigroup (..), show)
 
 ------------------------------------------------------------
 
 -- | Parameter of endpoints
--- type CreateChest = Value
--- type AddTreasure = Value
-
-type MorbidSchema =
-        Endpoint "Create Chest" ()
-        .\/ Endpoint "Unlock Chest" Value
-
--- | Tranche of a vesting scheme.
-data MorbidTranche = MorbidTranche {
-    morbidTrancheDate   :: POSIXTime,
-    morbidTrancheAmount :: Value
-    } deriving Generic
-
-PlutusTx.makeLift ''MorbidTranche
-
--- | A vesting scheme consisting of two tranches. Each tranche defines a date
---   (POSIX time) after which an additional amount can be spent.
-data MorbidParams = MorbidParams {
-    morbidTranche1 :: MorbidTranche,
-    morbidTranche2 :: MorbidTranche,
-    morbidOwner    :: PaymentPubKeyHash
-    } deriving Generic
-
-PlutusTx.makeLift ''MorbidParams
-
-{-# INLINABLE totalAmount #-}
--- | The total amount vested
-totalAmount :: MorbidParams -> Value
-totalAmount MorbidParams{morbidTranche1,morbidTranche2} =
-    morbidTrancheAmount morbidTranche1 + morbidTrancheAmount morbidTranche2
-
-{-# INLINABLE availableFrom #-}
--- | The amount guaranteed to be available from a given tranche in a given time range.
-availableFrom :: MorbidTranche -> POSIXTimeRange -> Value
-availableFrom (MorbidTranche d v) range =
-    -- The valid range is an open-ended range starting from the tranche vesting date
-    let validRange = Interval.from d
-    -- If the valid range completely contains the argument range (meaning in particular
-    -- that the start time of the argument range is after the tranche vesting date), then
-    -- the money in the tranche is available, otherwise nothing is available.
-    in if validRange `Interval.contains` range then v else zero
-
-availableAt :: MorbidParams -> POSIXTime -> Value
-availableAt MorbidParams{morbidTranche1, morbidTranche2} sl =
-    let f MorbidTranche{morbidTrancheDate, morbidTrancheAmount} =
-            if sl >= morbidTrancheDate then morbidTrancheAmount else mempty
-    in foldMap f [morbidTranche1, morbidTranche2]
-
-{-# INLINABLE remainingFrom #-}
--- | The amount that has not been released from this tranche yet
-remainingFrom :: MorbidTranche -> POSIXTimeRange -> Value
-remainingFrom t@MorbidTranche{morbidTrancheAmount} range =
-    morbidTrancheAmount - availableFrom t range
-
-{-# INLINABLE validate #-}
-validate :: MorbidParams -> () -> () -> ScriptContext -> Bool
-validate MorbidParams{morbidTranche1, morbidTranche2, morbidOwner} () () ctx@ScriptContext{scriptContextTxInfo=txInfo@TxInfo{txInfoValidRange}} =
-    let
-        remainingActual  = Validation.valueLockedBy txInfo (Validation.ownHash ctx)
-
-        remainingExpected =
-            remainingFrom morbidTranche1 txInfoValidRange
-            + remainingFrom morbidTranche2 txInfoValidRange
-
-    in remainingActual `Value.geq` remainingExpected
-            -- The policy encoded in this contract
-            -- is "vestingOwner can do with the funds what they want" (as opposed
-            -- to "the funds must be paid to vestingOwner"). This is enforcey by
-            -- the following condition:
-            && Validation.txSignedBy txInfo (unPaymentPubKeyHash morbidOwner)
-            -- That way the recipient of the funds can pay them to whatever address they
-            -- please, potentially saving one transaction.
+type CreateChest = Value
+type AddTreasure = Value
+type DelayUnlock = ()
+type UnlockChest = ()
 
 data Morbid
 instance Scripts.ValidatorTypes Morbid where
-    type instance RedeemerType Morbid = ()
-    type instance DatumType Morbid = ()
+    type instance DatumType     Morbid = ()
+    type instance RedeemerType  Morbid = ()
 
-morbidScript :: MorbidParams -> Validator
-morbidScript = Scripts.validatorScript . typedValidator
+{-# INLINABLE validate #-}
+validate :: () -> () -> ScriptContext -> Bool
+validate _ _ sc = True
 
-typedValidator :: MorbidParams -> Scripts.TypedValidator Morbid
-typedValidator = Scripts.mkTypedValidatorParam @Morbid
-    $$(PlutusTx.compile [|| validate ||])
-    $$(PlutusTx.compile [|| wrap ||])
-    where
-        wrap = Scripts.wrapValidator
+typedValidator :: Scripts.TypedValidator Morbid
+typedValidator = Scripts.mkTypedValidator @Morbid
+    $$(PlutusTx.compile [||         validate      ||])
+    $$(PlutusTx.compile [|| Scripts.wrapValidator ||])
 
-contractAddress :: MorbidParams -> Ledger.Address
-contractAddress = Scripts.validatorAddress . typedValidator
+contractAddress :: Ledger.Address
+contractAddress = Scripts.validatorAddress typedValidator
 
-morbidContract :: MorbidParams -> Contract () MorbidSchema T.Text ()
-morbidContract morbid = selectList [chest, retrieve]
-  where
-    chest = endpoint @"Create Chest" $ \_ -> chestFundsC morbid
-    retrieve = endpoint @"Unlock Chest" $ \payment -> do
-        liveness <- retrieveFundsC morbid payment
-        case liveness of
-            Alive -> awaitPromise retrieve
-            Dead  -> pure ()
+morbidContract :: AsContractError x => Contract () MorbidSchema x ()
+morbidContract = selectList
+    [ createChest
+    , addTreasure
+    , delayUnlock
+    , unlockChest
+    ]
 
-payIntoContract :: Value -> TxConstraints () ()
-payIntoContract = mustPayToTheScript ()
+-- | The "Create Chest" contract endpoint
+createChest :: AsContractError x => Promise () MorbidSchema x ()
+createChest = endpoint @"1. Create Chest" $ \ initialDeposit -> do
+    let tvl = Constraints.typedValidatorLookups typedValidator
+        txn = Constraints.mustPayToTheScript () initialDeposit
+    constraints <- mkTxConstraints tvl txn
+    
+    let aut = Constraints.adjustUnbalancedTx constraints
+    void $ submitUnbalancedTx aut
 
-chestFundsC
-    :: MorbidParams
-    -> Contract () s T.Text ()
-chestFundsC morbid = do
-    let txn = payIntoContract (totalAmount morbid)
-    mkTxConstraints (Constraints.typedValidatorLookups $ typedValidator morbid) txn
-      >>= void . submitUnbalancedTx . Constraints.adjustUnbalancedTx
+-- | The "Add Treasure" contract endpoint.
+addTreasure :: AsContractError x => Promise () MorbidSchema x ()
+addTreasure = endpoint @"2. Add Treasure" $ \ deposit -> do
+    -- let tvl = Constraints.typedValidatorLookups typedValidator
+    --     txn = Constraints.mustPayToTheScript () deposit
+    -- constraints <- mkTxConstraints tvl txn
+    
+    -- let aut = Constraints.adjustUnbalancedTx constraints
+    -- void $ submitUnbalancedTx aut
+    error ()
 
-data Liveness = Alive | Dead
+-- | The "Delay Unlock" contract endpoint.
+delayUnlock :: AsContractError x => Promise () MorbidSchema x ()
+delayUnlock = endpoint @"3. Delay Unlock" $ \ _ -> do
+    -- utxoS  <- utxosAt contractAddress
+    
+    -- let txn = collectFromScript utxoS ()
+    -- void $ submitTxConstraintsSpending typedValidator utxoS txn
+    error ()
 
-retrieveFundsC
-    :: MorbidParams
-    -> Value
-    -> Contract () s T.Text Liveness
-retrieveFundsC morbid payment = do
-    let inst = typedValidator morbid
-        addr = Scripts.validatorAddress inst
-    nextTime <- awaitTime 0
-    unspentOutputs <- utxosAt addr
-    let
-        currentlyLocked = foldMap (view Tx.ciTxOutValue) (Map.elems unspentOutputs)
-        remainingValue = currentlyLocked - payment
-        mustRemainLocked = totalAmount morbid - availableAt morbid nextTime
-        maxPayment = currentlyLocked - mustRemainLocked
+-- | The "Unlock Chest" contract endpoint.
+unlockChest :: AsContractError x => Promise () MorbidSchema x ()
+unlockChest = endpoint @"4. Unlock Chest" $ \ _ -> do
+    utxoS  <- utxosAt contractAddress
+    
+    let txn = collectFromScript utxoS ()
+    void $ submitTxConstraintsSpending typedValidator utxoS txn
 
-    when (remainingValue `Value.lt` mustRemainLocked)
-        $ throwError
-        $ T.unwords
-            [ "Cannot take out"
-            , T.pack (show payment) `T.append` "."
-            , "The maximum is"
-            , T.pack (show maxPayment) `T.append` "."
-            , "At least"
-            , T.pack (show mustRemainLocked)
-            , "must remain locked by the script."
-            ]
+endpoints :: AsContractError x => Contract () MorbidSchema x ()
+endpoints = morbidContract
 
-    let liveness = if remainingValue `Value.gt` mempty then Alive else Dead
-        remainingOutputs = case liveness of
-                            Alive -> payIntoContract remainingValue
-                            Dead  -> mempty
-        txn = Typed.collectFromScript unspentOutputs ()
-                <> remainingOutputs
-                <> mustValidateIn (Interval.from nextTime)
-                <> mustBeSignedBy (morbidOwner morbid)
-                -- we don't need to add a pubkey output for 'vestingOwner' here
-                -- because this will be done by the wallet when it balances the
-                -- transaction.
-    mkTxConstraints (Constraints.typedValidatorLookups inst
-                  <> Constraints.unspentOutputs unspentOutputs) txn
-      >>= void . submitUnbalancedTx . Constraints.adjustUnbalancedTx
-    return liveness
-
-endpoints :: Contract () MorbidSchema T.Text ()
-endpoints = morbidContract morbidParams
-  where
-    morbidOwner = mockWalletPaymentPubKeyHash w1
-    morbidParams =
-        MorbidParams {morbidTranche1, morbidTranche2, morbidOwner}
-    morbidTranche1 =
-        MorbidTranche
-            {morbidTrancheDate = TimeSlot.scSlotZeroTime def + 20000, morbidTrancheAmount = Ada.lovelaceValueOf 50_000_000}
-    morbidTranche2 =
-        MorbidTranche
-            {morbidTrancheDate = TimeSlot.scSlotZeroTime def + 40000, morbidTrancheAmount = Ada.lovelaceValueOf 30_000_000}
-
+type MorbidSchema = Endpoint "1. Create Chest" CreateChest
+                .\/ Endpoint "2. Add Treasure" AddTreasure
+                .\/ Endpoint "3. Delay Unlock" DelayUnlock
+                .\/ Endpoint "4. Unlock Chest" UnlockChest
 mkSchemaDefinitions ''MorbidSchema
 
 $(mkKnownCurrencies [])
--- usecases/MorbidSimulations.hs
+++ usecases/MorbidSimulations.hs
@@ -2,6 +2,9 @@
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE OverloadedStrings  #-}
 
+{-# OPTIONS_GHC -fno-ignore-interface-pragmas #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+
 module MorbidSimulations where
 
 import Ledger.Ada            (lovelaceValueOf)
@@ -22,43 +25,59 @@ import SimulationUtils       (simulatorWallet)
 
 import Wallet.Emulator.Types (WalletNumber (..))
 
+createChest :: WalletNumber -> Value -> SimulatorAction
+createChest caller = callEndpoint caller "1. Create Chest"
+
+addTreasure :: WalletNumber -> Value -> SimulatorAction
+addTreasure caller = callEndpoint caller "2. Add Treasure"
+
+delayUnlock :: WalletNumber -> SimulatorAction
+delayUnlock caller = callEndpoint caller "3. Delay Unlock" ()
+
+unlockChest :: WalletNumber -> SimulatorAction
+unlockChest caller = callEndpoint caller "4. Unlock Chest" ()
+
 simulations :: [Simulation]
-simulations = [basicMorbid, badGuess]
+simulations = [davyJonesLocker, tooSoon]
   where
     wallet1 = WalletNumber 1
     wallet2 = WalletNumber 2
     wallet3 = WalletNumber 3
-    basicMorbid =
+    davyJonesLocker =
         Simulation
-            { simulationName = "Basic Morbid"
+            { simulationName = "Davy Jones' Locker"
             , simulationId = 1
             , simulationWallets = simulatorWallet registeredKnownCurrencies 100_000_000 <$> [wallet1, wallet2, wallet3]
             , simulationActions =
-                  [ chestFunds wallet2
-                  , AddBlocks 20
-                  , unlockChest wallet1 (lovelaceValueOf 40_000_000)
-                  , AddBlocks 40
-                  , unlockChest wallet1 (lovelaceValueOf 40_000_000)
-                  , AddBlocks 1
+                  [ createChest wallet1 (lovelaceValueOf 50_000_000)
+                  , AddBlocks 100_000
+                  , addTreasure wallet1 (lovelaceValueOf 20_000_000)
+                  , AddBlocks 500_000
+                  , delayUnlock wallet1
+                  , AddBlocks 1_000_000
+                  , unlockChest wallet2
+                  , AddBlocks 1_000_000
+                  , unlockChest wallet3
+                  , AddBlocks 1_000
                   ]
             }
-    badGuess =
+    tooSoon =
         Simulation
-            { simulationName = "One Bad Guess"
+            { simulationName = "Too Soon"
             , simulationId = 2
             , simulationWallets = simulatorWallet registeredKnownCurrencies 100_000_000 <$> [wallet1, wallet2, wallet3]
             , simulationActions =
-                  [ chestFunds wallet2
-                  , AddBlocks 20
-                  , unlockChest wallet1 (lovelaceValueOf 40_000_000)
-                  , AddBlocks 40
-                  , unlockChest wallet1 (lovelaceValueOf 40_000_000)
+                  [ createChest wallet1 (lovelaceValueOf 50_000_000)
+                  , AddBlocks 100
+                  , addTreasure wallet1 (lovelaceValueOf 20_000_000)
+                  , AddBlocks 500
+                  , delayUnlock wallet1
+                  , AddBlocks 1_000
+                  , unlockChest wallet2
+                  , AddBlocks 1_000
+                  , unlockChest wallet3
                   , AddBlocks 1
                   ]
             }
-
-chestFunds :: WalletNumber -> SimulatorAction
-chestFunds caller = callEndpoint caller "Create Chest" ()
-
-unlockChest :: WalletNumber -> Value -> SimulatorAction
-unlockChest caller = callEndpoint caller "Unlock Chest"
+        
+    
