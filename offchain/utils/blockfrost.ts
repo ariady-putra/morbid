ce33858aae0fa3ba88ac249bf7f11f025c937436
Kiki Ariady Putra <aurodeus@gmail.com>
2023-07-05 00:36:25 +0700

    Get asset & script from tx

--- offchain/utils/blockfrost.ts
+++ offchain/utils/blockfrost.ts
@@ -31,3 +31,61 @@ export async function getDatumFields(datumHash: string) {
 
   return datumFields;
 }
+
+export async function getScriptCBOR(scriptHash: string) {
+  return (
+    await (
+      await fetch(
+        `https://cardano-preview.blockfrost.io/api/v0/scripts/${scriptHash}/cbor`,
+        {
+          headers: {
+            project_id: process.env.NEXT_PUBLIC_BLOCKFROST as string,
+          },
+        }
+      )
+    ).json()
+  )["cbor"];
+}
+
+export async function getTxUTxOs(txHash: string) {
+  return (
+    await fetch(
+      `https://cardano-preview.blockfrost.io/api/v0/txs/${txHash}/utxos`,
+      {
+        headers: {
+          project_id: process.env.NEXT_PUBLIC_BLOCKFROST as string,
+        },
+      }
+    )
+  ).json();
+}
+
+export async function getTxMetadata(txHash: string) {
+  const metadata = await fetch(
+    `https://cardano-preview.blockfrost.io/api/v0/txs/${txHash}/metadata`,
+    {
+      headers: {
+        project_id: process.env.NEXT_PUBLIC_BLOCKFROST as string,
+      },
+    }
+  );
+  const metadataJson = await metadata.json();
+  // console.log({ metadataJson: metadataJson[0] });
+  return metadataJson[0];
+}
+
+export async function getAssetMetadata(assetName: string) {
+  if (assetName === "lovelace") return;
+
+  const metadata = await fetch(
+    `https://cardano-preview.blockfrost.io/api/v0/assets/${assetName}`,
+    {
+      headers: {
+        project_id: process.env.NEXT_PUBLIC_BLOCKFROST as string,
+      },
+    }
+  );
+  const metadataJson = await metadata.json();
+  // console.log({ metadataJson: metadataJson });
+  return metadataJson;
+}
