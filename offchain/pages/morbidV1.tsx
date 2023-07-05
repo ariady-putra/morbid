0597c24a2fad5b0f88cb44c2e888d8cb69e8aad8
Kiki Ariady Putra <aurodeus@gmail.com>
2023-07-05 16:11:00 +0700

    Done v0.2.0 with initial integration tests

---
+++ offchain/pages/morbidV1.tsx
@@ -0,0 +1,65 @@
+import type { NextPage } from "next";
+import Router from "next/router";
+
+import { useState, useEffect } from "react";
+import { StoreProvider } from "easy-peasy";
+import { Lucid } from "lucid-cardano";
+
+import { useStoreState } from "../utils/store";
+import chestStore from "../utils/chest";
+import initLucid from "../utils/lucid";
+
+import MorbidV1 from "../components/MorbidV1";
+import NavBar from "../components/NavBar";
+import Title from "../components/Title";
+
+const Helios: NextPage = () => {
+  const MorbidProvider = StoreProvider as any;
+
+  const walletStore = useStoreState((state: any) => state.wallet);
+  const [lucid, setLucid] = useState<Lucid>();
+
+  const [actionResult, setActionResult] = useState("");
+
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
+  useEffect(() => {
+    if (walletStore.name == "") {
+      Router.push("/");
+      return;
+    }
+
+    if (lucid) {
+    } else {
+      initLucid(walletStore.name).then((Lucid: Lucid) => {
+        setLucid(Lucid);
+      });
+    }
+  }, [lucid]);
+
+  return !loaded || !lucid || walletStore.name == "" ? (
+    <></>
+  ) : (
+    <div className="px-10">
+      <Title />
+
+      <div className="navbar bg-base-100">
+        <NavBar />
+      </div>
+
+      <MorbidProvider store={chestStore}>
+        <MorbidV1 lucid={lucid} setActionResult={setActionResult} />
+      </MorbidProvider>
+
+      <div className="px-10 text-xl">
+        <pre>{actionResult}</pre>
+      </div>
+    </div>
+  );
+};
+
+export default Helios;
